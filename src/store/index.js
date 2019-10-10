8834ac076f4f3eeb11bfab7123489293503c7248
DavyGCLima <davy.g.c.lima>
2019-10-10 12:00:57 -0300

    reactotron implementado junto ao redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 
 import todos from './reducers/todo';
 
-const store = createStore(todos);
+const composer = process.env.NODE_ENV === 'development'
+  ? compose(applyMiddleware(...[]), console.tron.createEnhancer())
+  : applyMiddleware(...[]);
+
+const store = createStore(todos, composer);
 
 export default store;
